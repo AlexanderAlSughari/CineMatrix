a8fef3753b0cd70e90f145b5bc189b34b17cab0d
KacperKropacz <89976303+KacperKropacz@users.noreply.github.com>
2023-11-27 21:27:05 +0100

    walidacja pól tworzenia filmu
    została dodana walidacja pól formularza uzupełnianego podczas tworzenia nowego filmu

--- js/addMovie.js
+++ js/addMovie.js
@@ -1,3 +1,43 @@
+<<<<<<< HEAD
+const validate = () => {
+    let posterPath = document.getElementById('poster_path').value;
+    let title = document.getElementById('title').value;
+    let releaseDate = document.getElementById('release_date').value;
+    let runtime = document.getElementById('runtime').value;
+    let genres = document.getElementById('genres').value;
+    let overview = document.getElementById('overview').value;
+    let productionCountries = document.getElementById('production_countries').value;
+    let budget = document.getElementById('budget').value;
+
+    if (!posterPath || !title || !releaseDate || !runtime || !genres || !overview || !productionCountries || !budget) {
+        alert('Wszystkie pola muszą być uzupełnione!');
+        return;
+    }
+};
+
+const fs = require('fs');
+const addMovie = () => {
+    let movie = {
+        id: Date.now(),
+        poster_path: document.getElementById('poster_path').value,
+        title: document.getElementById('title').value,
+        release_date: document.getElementById('release_date').value,
+        runtime: document.getElementById('runtime').value,
+        genres: document.getElementById('genres').value,
+        overview: document.getElementById('overview').value,
+        production_countries: document.getElementById('production_countries').value,
+        budget: document.getElementById('budget').value
+    }
+    movies.push(movie);
+
+    const content = "movies";
+    fs.writeFile('js\\movies.txt', content, err => {
+        if(error) {
+            console.err;
+            return;
+        }
+    })
+=======
 const validate = () => {
     let posterPath = document.getElementById('poster_path').value;
     let title = document.getElementById('title').value;
@@ -35,4 +75,5 @@ const addMovie = () => {
             return;
         }
     })
+>>>>>>> e84b8a7db3bdfb046a52c6a5dd03d78f1fda19da
 }
