e84b8a7db3bdfb046a52c6a5dd03d78f1fda19da
KacperKropacz <89976303+KacperKropacz@users.noreply.github.com>
2023-11-27 21:17:03 +0100

    Add files via upload

--- js/addMovie.js
+++ js/addMovie.js
@@ -1,3 +1,18 @@
+const validate = () => {
+    let posterPath = document.getElementById('poster_path').value;
+    let title = document.getElementById('title').value;
+    let releaseDate = document.getElementById('release_date').value;
+    let runtime = document.getElementById('runtime').value;
+    let genres = document.getElementById('genres').value;
+    let overview = document.getElementById('overview').value;
+    let productionCountries = document.getElementById('production_countries').value;
+    let budget = document.getElementById('budget').value;
+
+    if (!posterPath || !title || !releaseDate || !runtime || !genres || !overview || !productionCountries || !budget) {
+        alert('Wszystkie pola muszą być uzupełnione!');
+        return;
+    }
+};
 const fs = require('fs');
 const addMovie = () => {
     let movie = {
