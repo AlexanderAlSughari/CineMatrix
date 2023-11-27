90a86fab25d3f84ec5c45c494271220052024aa6
Alexander Al-Sughari <89976396+AlexanderAlSughari@users.noreply.github.com>
2023-11-27 19:52:06 +0100

    Dodanie skryptu - sortowanie
    Dodanie skryptu do sortowania filmów wg. daty, tytułu oraz popularności
    Małe poprawki stylu listy rozwijanej

--- js/searchMovie.js
+++ js/searchMovie.js
@@ -9,11 +9,18 @@ const searchMoviesWithThatName = (keyword) => {
         return response.json();
     })
     .then((myJson) => {
+        let moviesArray = myJson.results;
         const genre = parseInt(document.getElementById('genres').value);
-        if(genre != 0)
-            moviesArray = filterMoviesByGenre(myJson.results, genre);
-        else
-            moviesArray = myJson.results;
+        const order = parseInt(document.getElementById('order').value);
+
+        moviesArray = (genre != 0) ? filterMoviesByGenre(moviesArray, genre) : moviesArray;
+
+        moviesArray = (order == 1) ? orderMoviesByPopularityDesc(moviesArray) : moviesArray;
+        moviesArray = (order == 2) ? orderMoviesByPopularityAsc(moviesArray) : moviesArray;
+        moviesArray = (order == 3) ? orderMoviesByDateDesc(moviesArray) : moviesArray;
+        moviesArray = (order == 4) ? orderMoviesByDateAsc(moviesArray) : moviesArray;
+        moviesArray = (order == 5) ? orderMoviesByTitleDesc(moviesArray) : moviesArray;
+        moviesArray = (order == 6) ? orderMoviesByTitleAsc(moviesArray) : moviesArray;
 
         if(moviesArray.length == 0)
             document.querySelector('.movies').innerHTML = "brak wyników :(";
@@ -36,4 +43,9 @@ const searchMoviesWithThatName = (keyword) => {
     .catch(err => console.error(err));
 }
 const filterMoviesByGenre = (list, chosenGenre) => list.filter(movie => movie.genre_ids.includes(chosenGenre));
-const sortMoviesIntoOrder = (list) => list.sort(movie => movie.genre_ids == chosenGenre);
+const orderMoviesByPopularityDesc = (list) => list.sort((a, b) => b.popularity - a.popularity);
+const orderMoviesByPopularityAsc = (list) => list.sort((a, b) => a.popularity - b.popularity);
+const orderMoviesByDateDesc = (list) => list.sort((a, b) => new Date(b.release_date) - new Date(a.release_date));
+const orderMoviesByDateAsc = (list) => list.sort((a, b) => new Date(a.release_date) - new Date(b.release_date));
+const orderMoviesByTitleAsc = (list) => list.sort((a, b) => a.title.localeCompare(b.title));
+const orderMoviesByTitleDesc = (list) => list.sort((a, b) => b.title.localeCompare(a.title));
