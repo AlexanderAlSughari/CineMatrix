d6428163394c9563097cfb1a3300921372a7b480
ZelazneKrzeslo <olikus.kusmierczyk@gmail.com>
2023-11-29 00:06:01 +0100

    statystyki
    Wyswietlanie najlepiej ocenianych filmów

---
+++ js/statistics.js
@@ -0,0 +1,35 @@
+fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
+    .then(res => {
+        return res.json();
+    })
+    .then((myJson) => {
+
+        const moviesSection = document.querySelector('.movies');
+
+        if (moviesSection) {
+            myJson.results.forEach(element => {
+
+                let movieSection = document.createElement('section');
+                movieSection.classList.add('movie');
+
+                let moviePoster = document.createElement('img');
+                moviePoster.src = path + element.poster_path;
+
+                let movieTitle = document.createElement('h3');
+                movieTitle.innerHTML = `Tytuł: ${element.title}`;
+
+                let movieRating = document.createElement('p');
+                movieRating.innerHTML = `Ocena: ${element.vote_average}`;
+
+                movieSection.appendChild(movieTitle);
+                movieSection.appendChild(movieRating);
+                movieSection.appendChild(moviePoster)
+
+                moviesSection.appendChild(movieSection);
+            });
+        } else {
+            console.error('Nie udało się znaleźć sekcji na stronie.');
+        }
+    })
+    .catch(error => console.log(error));
+
