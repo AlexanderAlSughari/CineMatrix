b403015077e36668aa6998b88798a3871a1ec08e
ZelazneKrzeslo <olikus.kusmierczyk@gmail.com>
2023-11-29 13:58:27 +0100

    statystyki
    poprawienie skryptu statystyk

--- js/statistics.js
+++ js/statistics.js
@@ -1,71 +1,145 @@
 fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
-    .then(res => {
-        return res.json();
-    })
-    .then((myJson) => {
+.then(response => response.json())
+.then((myJson) => {
+    let i = 1;
+    const moviesSection = document.querySelector('.movies-popularity');
+    if (moviesSection) {
+        let array = filterByPopularity(myJson.results).slice(0,5);
+        array.forEach(element => {
+            let movieSection = document.createElement('section');
+            movieSection.classList.add('movie');
 
-        const moviesSection = document.querySelector('.movies');
+            let moviePoster = document.createElement('img');
+            moviePoster.src = path + element.poster_path;
 
-        if (moviesSection) {
-            myJson.results.slice(0,5).forEach(element => {
+            let number = document.createElement('p');
+            number.innerHTML = '#' +  i++;
 
-                let movieSection = document.createElement('section');
-                movieSection.classList.add('movie');
+            let movieTitle = document.createElement('h3');
+            movieTitle.innerHTML = element.title;
 
-                let moviePoster = document.createElement('img');
-                moviePoster.src = path + element.poster_path;
+            let movieRating = document.createElement('span');
+            movieRating.classList.add('stats');
+            movieRating.innerHTML = 'Popularność: ' + element.popularity;
 
-                let movieTitle = document.createElement('h3');
-                movieTitle.innerHTML = `Tytuł: ${element.title}`;
+            movieSection.appendChild(moviePoster);
+            movieSection.appendChild(number);
+            movieSection.appendChild(movieTitle);
+            movieSection.appendChild(movieRating);
 
-                let movieRating = document.createElement('p');
-                movieRating.innerHTML = `Ocena: ${element.vote_average}`;
+            moviesSection.appendChild(movieSection);
+        })
+    }
+    else
+        console.error('Nie udało się znaleźć sekcji na stronie.');
+})
+.catch(error => console.log(error));
 
-                movieSection.appendChild(movieTitle);
-                movieSection.appendChild(movieRating);
-                movieSection.appendChild(moviePoster)
+fetch('https://api.themoviedb.org/3/tv/top_rated?language=en-US&page=1', options)
+.then(response => response.json())
+.then((myJson) => {
+    let i = 1;
+    const moviesSection = document.querySelector('.series-popularity');
+    if (moviesSection) {
+        let array = filterByPopularity(myJson.results).slice(0,5);
+        array.forEach(element => {
+            let movieSection = document.createElement('section');
+            movieSection.classList.add('movie');
 
-                moviesSection.appendChild(movieSection);
-            });
-        } else {
-            console.error('Nie udało się znaleźć sekcji na stronie.');
-        }
-    })
-    .catch(error => console.log(error));
+            let moviePoster = document.createElement('img');
+            moviePoster.src = path + element.poster_path;
 
-    fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=449', options)
-    .then(res => {
-        return res.json();
-    })
-    .then((myJson) => {
+            let number = document.createElement('p');
+            number.innerHTML = '#' +  i++;
 
-        const moviesSection = document.querySelector('.movies1');
+            let movieTitle = document.createElement('h3');
+            movieTitle.innerHTML = element.name;
 
-        if (moviesSection) {
-            const lowestRated = myJson.results.reverse().slice(0, 5);
+            let movieRating = document.createElement('span');
+            movieRating.innerHTML = 'Popularnność: ' + element.popularity;
 
-            lowestRated.forEach(element => {
+            movieSection.appendChild(moviePoster);
+            movieSection.appendChild(number);
+            movieSection.appendChild(movieTitle);
+            movieSection.appendChild(movieRating);
 
-                let movieSection = document.createElement('section');
-                movieSection.classList.add('movie');
+            moviesSection.appendChild(movieSection);
+        });
+    } else
+        console.error('Nie udało się znaleźć sekcji na stronie.');
+})
+.catch(error => console.log(error));
 
-                let moviePoster = document.createElement('img');
-                moviePoster.src = path + element.poster_path;
+fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
+.then(response => response.json())
+.then((myJson) => {
+    let i = 1;
+    const moviesSection = document.querySelector('.movies-unpopularity');
+    if (moviesSection) {
+        let array = filterByUnpopularity(myJson.results).slice(0,5);
+        array.forEach(element => {
+            let movieSection = document.createElement('section');
+            movieSection.classList.add('movie');
 
-                let movieTitle = document.createElement('h3');
-                movieTitle.innerHTML = `Tytuł: ${element.title}`;
+            let moviePoster = document.createElement('img');
+            moviePoster.src = path + element.poster_path;
 
-                let movieRating = document.createElement('p');
-                movieRating.innerHTML = `Ocena: ${element.vote_average}`;
+            let number = document.createElement('p');
+            number.innerHTML = '#' +  i++;
 
-                movieSection.appendChild(movieTitle);
-                movieSection.appendChild(movieRating);
-                movieSection.appendChild(moviePoster)
+            let movieTitle = document.createElement('h3');
+            movieTitle.innerHTML = element.name;
 
-                moviesSection.appendChild(movieSection);
-            });
-        } else {
-            console.error('Nie udało się znaleźć sekcji na stronie.');
-        }
-    })
-    .catch(error => console.log(error));
+            let movieRating = document.createElement('span');
+            movieRating.innerHTML = 'Popularnność: ' + element.popularity;
+
+            movieSection.appendChild(moviePoster);
+            movieSection.appendChild(number);
+            movieSection.appendChild(movieTitle);
+            movieSection.appendChild(movieRating);
+
+            moviesSection.appendChild(movieSection);
+        });
+    } else
+        console.error('Nie udało się znaleźć sekcji na stronie.');
+})
+.catch(error => console.log(error));
+
+fetch('https://api.themoviedb.org/3/tv/top_rated?language=en-US&page=1', options)
+.then(response => response.json())
+.then((myJson) => {
+    let i = 1;
+    console.log(myJson.results)
+    const moviesSection = document.querySelector('.series-unpopularity');
+    if (moviesSection) {
+        let array = filterByUnpopularity(myJson.results).slice(0,5);
+        array.forEach(element => {
+            let movieSection = document.createElement('section');
+            movieSection.classList.add('movie');
+
+            let moviePoster = document.createElement('img');
+            moviePoster.src = path + element.poster_path;
+
+            let number = document.createElement('p');
+            number.innerHTML = '#' +  i++;
+
+            let movieTitle = document.createElement('h3');
+            movieTitle.innerHTML = element.name;
+
+            let movieRating = document.createElement('span');
+            movieRating.innerHTML = 'Popularnność: ' + element.popularity;
+
+            movieSection.appendChild(moviePoster);
+            movieSection.appendChild(number);
+            movieSection.appendChild(movieTitle);
+            movieSection.appendChild(movieRating);
+
+            moviesSection.appendChild(movieSection);
+        });
+    } else
+        console.error('Nie udało się znaleźć sekcji na stronie.');
+})
+.catch(error => console.log(error));
+
+const filterByPopularity = (list) => list.sort((a, b) => b.popularity - a.popularity);
+const filterByUnpopularity = (list) => list.sort((a, b) => a.popularity - b.popularity);
