121fd2df77fb27f73a6b25eabda5516b7c149316
KacperKropacz <89976303+KacperKropacz@users.noreply.github.com>
2023-11-29 12:58:52 +0100

    statystyki
    dodanie statystyk najnizej ocenianych filmow

--- js/statistics.js
+++ js/statistics.js
@@ -7,7 +7,7 @@ fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', opti
         const moviesSection = document.querySelector('.movies');
 
         if (moviesSection) {
-            myJson.results.forEach(element => {
+            myJson.results.slice(0,5).forEach(element => {
 
                 let movieSection = document.createElement('section');
                 movieSection.classList.add('movie');
@@ -33,3 +33,39 @@ fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', opti
     })
     .catch(error => console.log(error));
 
+    fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=449', options)
+    .then(res => {
+        return res.json();
+    })
+    .then((myJson) => {
+
+        const moviesSection = document.querySelector('.movies1');
+
+        if (moviesSection) {
+            const lowestRated = myJson.results.reverse().slice(0, 5);
+
+            lowestRated.forEach(element => {
+
+                let movieSection = document.createElement('section');
+                movieSection.classList.add('movie');
+
+                let moviePoster = document.createElement('img');
+                moviePoster.src = path + element.poster_path;
+
+                let movieTitle = document.createElement('h3');
+                movieTitle.innerHTML = `Tytuł: ${element.title}`;
+
+                let movieRating = document.createElement('p');
+                movieRating.innerHTML = `Ocena: ${element.vote_average}`;
+
+                movieSection.appendChild(movieTitle);
+                movieSection.appendChild(movieRating);
+                movieSection.appendChild(moviePoster)
+
+                moviesSection.appendChild(movieSection);
+            });
+        } else {
+            console.error('Nie udało się znaleźć sekcji na stronie.');
+        }
+    })
+    .catch(error => console.log(error));
